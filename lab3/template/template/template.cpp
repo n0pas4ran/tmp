7ce2fff6e0b4a708c5cf958643236e1b35540c40
n0pas4ran <ilya.tolkunov@mail.ru>
2023-05-20 14:33:52 +0300

    add strategy

---
+++ lab3/template/template/template.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base
+{
+    void a()
+    {
+        cout << "a  ";
+    }
+    void c()
+    {
+        cout << "c  ";
+    }
+    void e()
+    {
+        cout << "e  ";
+    }
+    
+    virtual void ph1() = 0;
+    virtual void ph2() = 0;
+public:
+    
+    void execute()
+    {
+        a();
+        ph1();
+        c();
+        ph2();
+        e();
+    }
+};
+
+class One : public Base
+{
+    /*virtual*/void ph1()
+    {
+        cout << "b  ";
+    }
+    /*virtual*/void ph2()
+    {
+        cout << "d  ";
+    }
+};
+
+class Two : public Base
+{
+    /*virtual*/void ph1()
+    {
+        cout << "2  ";
+    }
+    /*virtual*/void ph2()
+    {
+        cout << "4  ";
+    }
+};
+
+int main()
+{
+    Base* array[] =
+    {
+       new One(), new Two()
+    };
+    for (int i = 0; i < 2; i++)
+    {
+        array[i]->execute();
+        cout << '\n';
+    }
+}
